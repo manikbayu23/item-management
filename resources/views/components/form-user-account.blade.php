72507e63492b54c8eb2b8c8843d1fcead3a5e424
manikbayu23 <gdmanikmas@gmail.com>
2025-08-27 21:50:53 +0800

    update revision

--- resources/views/components/form-user-account.blade.php
+++ resources/views/components/form-user-account.blade.php
@@ -87,11 +87,13 @@
         <div class="col-12 col-md-9">
             <select name="role" id="role" class="form-control select" data-placeholder="Pilih Role...">
                 <option></option>
-                <option value="admin" @if ('admin' == old('role', isset($user->role) ? $user->role : '')) selected @endif>
-                    Admin
-                </option>
-                <option value="pic" @if ('pic' == old('role', isset($user->role) ? $user->role : '')) selected @endif>PIC
-                </option>
+                @if (Auth::user()->role == 'admin')
+                    <option value="admin" @if ('admin' == old('role', isset($user->role) ? $user->role : '')) selected @endif>
+                        Admin
+                    </option>
+                    <option value="pic" @if ('pic' == old('role', isset($user->role) ? $user->role : '')) selected @endif>PIC
+                    </option>
+                @endif
                 <option value="staff" @if ('staff' == old('role', isset($user->role) ? $user->role : '')) selected @endif>Staf
                 </option>
             </select>
